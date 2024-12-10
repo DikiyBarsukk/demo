1994397c8a8abe752d45fcba78bb1ea462846697
DikiyBarsyk <145682865+DikiyBarsyk@users.noreply.github.com>
2024-12-10 17:30:36 +0300

    Commit

--- src/main/java/ru/mtuci/rbpo_2024_praktika/controller/DeviceController.java
+++ src/main/java/ru/mtuci/rbpo_2024_praktika/controller/DeviceController.java
@@ -1,12 +1,12 @@
 package ru.mtuci.rbpo_2024_praktika.controller;
 
 import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.*;
+import ru.mtuci.rbpo_2024_praktika.controller.dto.AddDeviceDTO;
 import ru.mtuci.rbpo_2024_praktika.model.User;
 import ru.mtuci.rbpo_2024_praktika.service.DeviceService;
 import ru.mtuci.rbpo_2024_praktika.service.UserService;
